f24690ca6dd59b34f49b63a4be6a73c5bc9f3479
Neil Mastroianni <nf_mastroianni@hotmail.com>
2023-09-13 21:04:56 -0400

    added mobile styles

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -14,30 +14,32 @@ interface SliderProps extends CarouselSlice {
 export default function Slider({ content, primary }: SliderProps) {
   const targetRef = useRef<HTMLDivElement | null>(null)
   const { scrollYProgress } = useScroll({ target: targetRef })
-  const x = useTransform(scrollYProgress, [0, 1], ['5%', '-80%'])
+  const x = useTransform(scrollYProgress, [0, 1], ['5%', '-95%'])
   return (
     <div ref={targetRef} className="relative h-[300vh]">
       <div className="h-screen sticky top-0 flex items-center overflow-hidden">
         <div className="top-16 absolute left-1/2 -translate-x-1/2 w-full text-center">
           <PrismicRichText field={primary.heading} />
         </div>
-        <motion.div style={{ x }} className="flex gap-4">
+        <motion.div style={{ x }} className="flex gap-8">
           {content &&
             content.length > 0 &&
             content.map((item, i) => {
               return (
                 <div
                   key={item.id}
-                  className="grid gap-x-4 grid-cols-3 w-[360px] lg:w-[900px]"
+                  className="grid gap-x-4 lg:grid-cols-3 w-[300px] lg:w-[900px] shadow border rounded-lg"
                 >
-                  <div className="col-span-1">
+                  <div className="lg:block col-span-1 hidden">
                     <PrismicNextImage
                       field={item.data.image}
-                      className="rounded-l-lg"
+                      className="lg:rounded-l-lg"
                     />
                   </div>
-                  <blockquote className="col-span-2">
+                  <blockquote className="col-span-2 relative flex flex-col justify-between prose lg:prose-lg xl:prose-xl p-6">
+                    <FaQuoteLeft className="w-4 h-4 text-color-primary lg:w-12 lg:h-12" />
                     <PrismicRichText field={item.data.text} />
+                    <footer>{'-' + item.data.attribution}</footer>
                   </blockquote>
                 </div>
               )
