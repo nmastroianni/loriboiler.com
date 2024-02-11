789daa9166d4b7104cdfd0dd1e5579c720adb524
Neil Mastroianni <nf_mastroianni@hotmail.com>
2024-02-11 15:32:57 -0500

    updated packages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   trailingSlash: false,
   images: {
-    domains: [
-      'images.prismic.io',
-      'images.unsplash.com',
-      'prismic-io.s3.amazonaws.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.prismic.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prismic-io.s3.amazonaws.com',
+      },
     ],
   },
 }
